b896d716dcc747e96c3e13b6a08f803734b5d228
Anastasiia Nikulina <esther.morwell@gmail.com>
2022-05-17 14:43:49 +0300

    Lesson 20 mid commit

---
+++ Lesson20 DOM Forms/app/index.js
@@ -0,0 +1,142 @@
+const requester = document.forms.requester;
+
+const password = requester.password;
+const email = requester.email;
+const confirmPas = requester.confirm;
+const consent = requester.consent;
+
+
+const autofocus = (element) => {
+  if (element instanceof HTMLElement) {
+    element.focus();
+  }
+};
+
+autofocus(email);
+
+requester.addEventListener("submit", (event) => {
+  const result = document.querySelector(".result");
+  event.preventDefault();
+  /*   window.history.back(); */
+
+  if (checkError()) {
+
+    console.log("Error");
+
+  }
+
+  const form = new FormData(event.target);
+  const resultObj = {};
+  Array.from(form.keys())
+    .filter(key => form.get(key) != "").forEach(key =>
+      result.innerHTML = result.innerHTML.concat(` ${key}: ${form.get(key)}; `))
+   ;
+});
+
+
+
+validatePassword = (password, confirm) => {
+  let hasErrors = false;
+  if (password.value === "") {
+    createErrorField(password, "Cannot be empty");
+    hasErrors = true;
+  }
+  if (password.value.length < 9 || password.value.length > 24) {
+    createErrorField(password, "Wrong length");
+    hasErrors = true;
+
+  }
+  if (confirm.value === "") {
+    createErrorField(confirm, "Cannot be empty");
+    hasErrors = true;
+    return hasErrors;
+  }
+
+  if (confirm.value !== password.value) {
+    createErrorField(confirm, "Passwords do not match");
+    hasErrors = true;
+  }
+  return hasErrors;
+
+}
+
+validateEmail = (target) => {
+  let hasErrors = false;
+  if (target.value === "") {
+    createErrorField(target, "Cannot be empty");
+    hasErrors = true;
+    return hasErrors;
+  }
+
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (!target.value.match(validRegex)) {
+    createErrorField(target, "Wrong email");
+    hasErrors = true;
+  }
+  return hasErrors;
+}
+
+validateConsent = (target) => {
+  let hasErrors = false
+  if (!target.checked) {
+    createErrorField(target, "You must agree with our rules");
+    hasErrors = true;
+  }
+  return hasErrors;
+}
+
+checkError = () => {
+  const errors = requester.querySelectorAll('.error');
+
+  for (let i = 0; i < errors.length; i++) {
+    errors[i].innerHTML = "";
+  }
+  let hasErrors = validateEmail(email);
+  hasErrors = validatePassword(password, confirmPas) || hasErrors;
+  hasErrors = validateConsent(consent) || hasErrors;
+  return hasErrors;
+}
+
+
+const createErrorField = (input, text) => {
+  const error = input.parentElement.querySelector('span[role="alert"]');
+  error.hidden = false;
+  error.classList.add("error");
+  error.innerHTML = text;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
